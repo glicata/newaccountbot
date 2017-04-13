c6235cf28c702d24f923ef8dade00190b3a07f63
glicata <glicata@rightanswers.com>
2017-04-13 11:30:20 -0400

    Updated Files

--- messages/index.js
+++ messages/index.js
@@ -33,7 +33,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
     */
 
- .matches('new account' , [
+ intents.matches('new account' , [
     
       
     ]);
