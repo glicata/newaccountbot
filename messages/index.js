8c2c6ed7b32945e1e4a1695381034824cbdf2de3
glicata <glicata@rightanswers.com>
2017-04-13 15:11:27 -0400

    Updated index.js

--- messages/index.js
+++ messages/index.js
@@ -34,8 +34,8 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
     */
 
-    .matches('new account', (session, args) =>
-    {
+    .matches('new account', (session, args) => {
+       
         /*
         function(session, args, next) {
             var accountType = builder.EntityRecognizer.findEntity(args.entities, 'accountType');
@@ -55,7 +55,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
                 next();
             }
         }
-       */ 
+       */
 
     })
 
