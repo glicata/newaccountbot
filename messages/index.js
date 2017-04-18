ece5e7674b5b1cb0224ff2b68e21809a6c95162e
gilicata12 <licata.giuseppe.c@gmail.com>
2017-04-18 10:48:52 -0400

    updated index

--- messages/index.js
+++ messages/index.js
@@ -30,7 +30,7 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-
+bot.dialog('/', intents);
     /*
     .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
     */
@@ -48,7 +48,7 @@ intents.matches('new account', [
     session.send('Sorry,  did not understand \'%s\'.', session.message.text);
     });
 
-bot.dialog('/', intents);    
+    
 
 if (useEmulator) {
     var restify = require('restify');
