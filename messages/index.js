ba72d0e6890fc4648b749ad9c977318ab5abb9e4
gilicata12 <licata.giuseppe.c@gmail.com>
2017-04-18 09:42:50 -0400

    Updated index

--- messages/index.js
+++ messages/index.js
@@ -38,6 +38,19 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('new account', (session) => {
 
             console.log(args);
+            //session.dialogData.args = args;
+            var accountType = builder.EntityRecognizer.findEntity(args.entities, 'accountType');
+            var accountLevel = builder.EntityRecognizer.findEntity(args.entities, 'accountLevel');
+            var typeOfPersonalAccount = builder.EntityRecognizer.findEntity(args.entities,'accountType::typeOfPersonalAccount');
+            var typeOfBusinessAccount = builder.EntityRecognizer.findEntity(args.entities,'accountType::typeOfBusiness');
+            console.log('ENTITIES', accountType, accountLevel, typeOfPersonalAccount, typeOfBusinessAccount);
+            var account = {
+                accountType: accountType ? accountType.entity : null,
+                accountLevel: accountLevel ? accountLevel.entity : null,
+                typeOfPersonalAccount: typeOfPersonalAccount ? typeOfPersonalAccount.entity : null,
+                typeOfBusinessAccount: typeOfBusinessAccount ? typeOfBusinessAccount.entity : null
+            }
+            session.dialogData.account = account;
 
             // Prompt for account type
             //var accountTypes = ["Business", "Personal"];
