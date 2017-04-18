ed97758b40a62f86b197c45d2fce4f30cd067516
gilicata12 <licata.giuseppe.c@gmail.com>
2017-04-18 09:32:52 -0400

    Updated index

--- messages/index.js
+++ messages/index.js
@@ -35,7 +35,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
     */
 
-    .matches('new account', => {
+    .matches('new account', (session) => {
 
             console.log(args);
             //session.dialogData.args = args;
